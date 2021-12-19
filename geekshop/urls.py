22cf4df87fa53d15a85b6c134861ecf682f96290
AntonZykov85 <zakupki_pomosh@mail.ru>
2021-12-19 19:23:03 +0300

    lesson_2_part_2 done

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -28,11 +28,10 @@ urlpatterns = [
     path('users/', include('authapp.urls', namespace='authapp')),
     path('baskets/', include('baskets.urls', namespace='baskets')),
     path('admins/', include('admins.urls', namespace='admins')),
-
+    path('', include('social_django.urls', namespace='social')),
     # path('language_new/', include('django.conf.urls',namespace='language_new')),
     # path('lang/', set_language, name='language'),
     # path(r'^i18n/', include('django.conf.urls.i18n')),
-    path(r'^i18n/', include('django.conf.urls.i18n')),
 
 ]
 
