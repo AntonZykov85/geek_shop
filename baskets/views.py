36da028fd47182ec3c90585a0fe3b56cd36ecb6f
AntonZykov85 <zakupki_pomosh@mail.ru>
2021-12-26 18:06:56 +0300

    lesson_3_part_2_done

--- baskets/views.py
+++ baskets/views.py
@@ -8,18 +8,17 @@ from django.template.loader import render_to_string
 from baskets.models import Basket
 from mainapp.models import Product
 
-@login_required
-def basket_add(request, id):
-     product = Product.objects.get(id=id)
-     baskets = Basket.objects.filter(user=request.user, product=product)
-     if not baskets.exist():
-         Basket.objects.create(user=request.user, product=product, quantity=1)
-         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-     else:
-         basket = baskets.first()
-         basket.quantity +=1
-         basket.save()
-     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+def basket_add(request,id):
+    user_select = request.user
+    product = Product.objects.get(id=id)
+    baskets = Basket.objects.filter(user=user_select,product=product)
+    if baskets:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+    else:
+        Basket.objects.create(user=user_select,product=product,quantity=1)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 @login_required
 def basket_remove(request,basket_id):
