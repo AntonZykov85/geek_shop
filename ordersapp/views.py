71fa108e2e4b0c5ac3a99a12a4520f9f3c0e527e
AntonZykov85 <zakupki_pomosh@mail.ru>
2021-12-26 18:35:42 +0300

    lesson_4_part_2_done

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -2,9 +2,9 @@ from django.db import transaction
 from django.db.models.signals import pre_save, pre_delete
 from django.dispatch import receiver
 from django.forms import inlineformset_factory
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
-
+from mainapp.models import Product
 # Create your views here.
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
@@ -44,7 +44,7 @@ class OrderCreate(CreateView, BaseClassContextMixin):
                     form.initial['product'] = basket_item[num].product
                     form.initial['quantity'] = basket_item[num].quantity
                     form.initial['price'] = basket_item[num].product.price
-                # basket_item.delete()
+                basket_item.delete()
             else:
                 formset = OrderFormSet()
         context['orderitems'] = formset
@@ -118,19 +118,27 @@ def order_forming_complete(request, pk):
     order.save()
     return HttpResponseRedirect(reverse('orders:list'))
 
-@receiver(pre_save, sender=Basket)
-@receiver(pre_save, sender=OrderItem)
-def product_quantity_update_save(sender, instance, **kwargs):
-    if instance.pk:
-        get_item = instance.get_item(int(instance.pk))
-        instance.product.quantity -= instance.quantity - get_item
-    else:
-        instance.product.quantity -= instance.quantity
-    instance.product.save()
+
+def get_product_price(request,pk):
+    if request.is_ajax():
+        product = Product.objects.get(pk=pk)
+        if product:
+            return JsonResponse({'price':product.price})
+        return JsonResponse({'price':0})
+
+#@receiver(pre_save, sender=Basket)
+#@receiver(pre_save, sender=OrderItem)
+#def product_quantity_update_save(sender, instance, **kwargs):
+ #   if instance.pk:
+ #       get_item = instance.get_item(int(instance.pk))
+ #       instance.product.quantity -= instance.quantity - get_item
+  #  else:
+ #       instance.product.quantity -= instance.quantity
+ #   instance.product.save()
 
 
-@receiver(pre_delete, sender=Basket)
-@receiver(pre_delete, sender=OrderItem)
-def product_quantity_update_delete(sender, instance, **kwargs):
-    instance.product.quantity += instance.quantity
-    instance.product.save()
+#@receiver(pre_delete, sender=Basket)
+#@receiver(pre_delete, sender=OrderItem)
+#def product_quantity_update_delete(sender, instance, **kwargs):
+#    instance.product.quantity += instance.quantity
+#    instance.product.save()
--- static/js/orders_script.js
+++ static/js/orders_script.js
@@ -54,7 +54,7 @@ window.onload = function () {
     });
 
 
-    function orderSummaryUpdate(orderitem_price, delta_quantity) {
+   function orderSummaryUpdate(orderitem_price, delta_quantity) {
         delta_cost = orderitem_price * delta_quantity;
         order_total_cost = Number((order_total_cost + delta_cost).toFixed(2));
         order_total_quantity = order_total_quantity + delta_quantity;
@@ -111,36 +111,36 @@ window.onload = function () {
 
 }
 
-    $('.order_form select').change(function () {
+ $('.order_form select').change(function () {
 
-          let target = event.target;
-          orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
-          let orderitem_product_pk = target.options[target.selectedIndex].value;
+        let target = event.target;
+        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
+        let orderitem_product_pk = target.options[target.selectedIndex].value;
 
-          console.log(orderitem_num)
-          console.log(orderitem_product_pk)
+        console.log(orderitem_num)
+        console.log(orderitem_product_pk)
 
-          if (orderitem_product_pk) {
-              $.ajax({
-                  url: '/orders/product/' + orderitem_product_pk + '/price/',
-                  success: function (data) {
-                      if (data.price) {
-                          price_arr[orderitem_num] = parseFloat(data.price)
-                          if (isNaN(quantity_arr[orderitem_num])) {
-                              quantity_arr[orderitem_num] = 0;
-                          }
-                          let price_html = '<span class="orderitems-' + orderitem_num + '-price">'
-                              + data.price.toString().replace('.', ',') + '</span> руб';
-                          let current_tr = $('.order_form table').find('tr:eq('+(orderitem_num+1)+')');
-                          current_tr.find('td:eq(2)').html(price_html)
-                      }
-                  }
+        if (orderitem_product_pk) {
+            $.ajax({
+                url: '/orders/product/' + orderitem_product_pk + '/price/',
+                success: function (data) {
+                    if (data.price) {
+                        price_arr[orderitem_num] = parseFloat(data.price)
+                        if (isNaN(quantity_arr[orderitem_num])) {
+                            quantity_arr[orderitem_num] = 0;
+                        }
+                        let price_html = '<span class="orderitems-' + orderitem_num + '-price">'
+                            + data.price.toString().replace('.', ',') + '</span> руб';
+                        let current_tr = $('.order_form table').find('tr:eq('+(orderitem_num+1)+')');
+                        current_tr.find('td:eq(2)').html(price_html)
+                    }
+                }
 
 
-              })
-          }
+            })
+        }
 
-      })
+    })
 
 window.onload = function () {
     $('.basket_list').on('click', 'input[type="number"]', function () {
