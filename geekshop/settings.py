3b453feb3f57a6e9079f5b5983f4e968e7f1dc59
AntonZykov85 <zakupki_pomosh@mail.ru>
2021-12-28 22:14:14 +0300

    lesson_5_part_2_done

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -84,17 +84,17 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 
 #DATABASES = {
 #    'default': {
- #       'ENGINE': 'django.db.backends.sqlite3',
- #       'NAME': BASE_DIR / 'db.sqlite3_old',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3_old',
 #    }
 #}
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'geekshop',
+       'NAME': 'geekshop',
         'USER': 'postgres'
-    }
+   }
 }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
