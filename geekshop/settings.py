d1a24cba5fca5505dbdc9f7d5aacdbb2b4bc0df3
AntonZykov85 <zakupki_pomosh@mail.ru>
2022-01-16 17:08:11 +0300

    lesson_7_p2_done

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.cache.UpdateCacheMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
