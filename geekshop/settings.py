a5dc92b3207cd85b8c1a0d3cfb8031e0d759bac1
AntonZykov85 <zakupki_pomosh@mail.ru>
2022-01-29 18:59:59 +0300

    lesson_8_done_2

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -217,15 +217,15 @@ if DEBUG:
         'template_profiler_panel.panels.template.TemplateProfilerPanel',
     ]
 
-# CACHE_MIDDLEWARE_ALIAS = 'default'
-# CACHE_MIDDLEWARE_SECONDS = 120
-# CACHE_MIDDLEWARE_KEY_PREFIX = 'geekbrains'
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 120
+CACHE_MIDDLEWARE_KEY_PREFIX = 'geekbrains'
+
+CACHES = {
+    'default':{
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '89.108.78.203:11211'
+    }
+}
 #
-# CACHES = {
-#     'default':{
-#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-#         'LOCATION': '89.108.78.203:11211'
-#     }
-# }
-# #
 LOW_CACHE = False
